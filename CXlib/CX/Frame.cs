cc2e57760bd5e55bda8379714a96c9aa6494dc72
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-03 10:29:44 +0800

    Create frame class.

---
+++ CXlib/CX/Frame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CXlib
+{
+    class Frame
+    {
+        public int MessageType { get; }
+        public int SequenceNumber { get; }
+        public string FunctionName { get; }
+        public string Payload { get; }
+        public Frame(int m, int i, string n, string o)
+        {
+            this.MessageType = m;
+            this.SequenceNumber = i;
+            this.FunctionName = n;
+            this.Payload = o;
+        }
+    }
+}
