1dba7142ee4096d729e6a499170211cf30c74610
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-03 10:33:02 +0800

    Create product class.

---
+++ CXlib/CX/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CXlib
+{
+    public class Product
+    {
+        public int ProductId { get; }
+        public string Symbol { get; }
+        public string ProductFullName { get; }
+        public string ProductType { get; }
+        public int DeciamPlaces { get; }
+        public Product(int product_id, string symbol, string product_full_name, string product_type, int decimal_places)
+        {
+            this.ProductId = product_id;
+            this.Symbol = symbol;
+            this.ProductFullName = product_full_name;
+            this.ProductType = product_type;
+            this.DeciamPlaces = decimal_places;
+        }
+    }
+}
