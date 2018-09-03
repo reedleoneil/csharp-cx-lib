1d245193b3f5aed37addbf55b2db66ebe98911ea
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-03 10:27:11 +0800

    Add source files.

---
+++ CXamples/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CXlib;
+
+namespace CXamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CX cx = new CX();
+        }
+    }
+}
