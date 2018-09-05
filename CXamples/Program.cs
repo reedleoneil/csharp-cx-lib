636a0e1fd1a00f4d734c43de1fbc96d35a7fc549
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-05 13:21:19 +0800

    Code refactor and add api calls.

--- CXamples/Program.cs
+++ CXamples/Program.cs
@@ -12,8 +12,43 @@ namespace CXamples
         static void Main(string[] args)
         {
             CX cx = new CX(CX.Production);
-            cx.GetProducts();
-            Console.ReadLine();
+            while (true)
+            {
+
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "get products":
+                        cx.GetProducts();
+                        break;
+                    case "get instruments":
+                        cx.GetInstruments();
+                        break;
+                    case "web authenticate user":
+                        cx.WebAuthenticateUser("user1", "password");
+                        break;
+                    case "get user accounts":
+                        cx.GetUserAccounts();
+                        break;
+                    case "get account transactions":
+                        cx.GetAccountTransactions(1, 200);
+                        break;
+                    case "get account positions":
+                        cx.GetAccountPositions(1);
+                        break;
+                    case "get account trades":
+                        cx.GetAccountTrades(1, 20, 0);
+                        break;
+                    case "send order":
+                        //cx.SendOrder();
+                        break;
+                    default:
+                        Console.WriteLine("Error: command not found.");
+                        break;
+                }
+            }
         }
+
+  
     }
 }
--- CXlib/CX.cs
+++ CXlib/CX.cs
@@ -11,29 +11,87 @@ namespace CXlib
     {
         public static string Production { get => "wss://api-cx.coins.asia/ws-api/"; }
         public static string Staging { get => "wss://api-cx.staging.coins.technology/ws-api/"; }
-        public List<Product> Products { get; }
-        public List<Instrument> Instruments { get; }
+        public bool IsConnected { get; private set; }
+        public bool IsAuthenticated { get; private set; }
+
+        public event EventHandler OnGetProducts;
+        public event EventHandler OnGetInstruments;
+        
         private WebSocket ws;
 
         public CX(string url)
         {
-            ws = new WebSocket(url);
-
-            ws.OnOpen += (sender, e) =>
-                Console.WriteLine("open");
-            ws.OnMessage += (sender, e) =>
-                Console.WriteLine("message " + e.Data);
-            ws.OnClose += (sender, e) =>
-               Console.WriteLine("close " + e.Reason);
-            ws.OnError += (sender, e) =>
-                Console.WriteLine("error " + e.Message);
-
-            ws.Connect();
+            ConnectToAPI(url);
         }
         
+        private void ConnectToAPI(string url)
+        {
+            ws = new WebSocket(url);
+            ws.OnOpen += (sender, e) =>
+            {
+                Console.WriteLine($"Websocket Open: {url}");
+                IsConnected = true;
+            };
+            ws.OnMessage += (sender, e) =>
+                Console.WriteLine($"Websocket Message {e.Data}");
+            ws.OnClose += (sender, e) =>
+            {
+                Console.WriteLine($"Websocket Close: {e.Code} {e.Reason}");
+                IsConnected = false;
+                ConnectToAPI(url);
+            };
+            ws.OnError += (sender, e) =>
+            {
+                Console.WriteLine($"Websocket Error: {e.Exception} {e.Message}");
+                IsConnected = false;
+                ConnectToAPI(url);
+            };
+            ws.Connect();
+        }
+
         public void GetProducts()
         {
             ws.Send("{\"m\":0,\"i\":0,\"n\":\"GetProducts\",\"o\":\"{\\\"OMSId\\\": 1}\"}");
+        }
+
+        public void GetInstruments()
+        {
+            ws.Send("{\"m\":0,\"i\":0,\"n\":\"GetInstruments\",\"o\":\"{\\\"OMSId\\\": 1}\"}");
+        }
+
+        public void WebAuthenticateUser(string username, string password)
+        {
+            ws.Send("{\"m\":0,\"i\":0,\"n\":\"WebAuthenticateUser\",\"o\":\"{\\\"UserName\\\":\\\"User1\\\",\\\"Password\\\":\\\"Password\\\"}\"}");
+        }
+
+        public void GetUserAccounts()
+        {
+            ws.Send("{\"m\":0,\"i\":0,\"n\":\"GetUserAccounts\",\"o\":\"\"}");
+        }
+
+        public void GetAccountTransactions(int accountId, int depth)
+        {
+            ws.Send("{\"m\":0,\"i\":0,\"n\":\"GetAccountTransactions\",\"o\":\"{\\\"OMSId\\\":1,\\\"AccountId\\\": 1,\\\"Depth\\\": 200}\"}");
+        }
+
+        public void GetAccountPositions(int accountId)
+        {
+            ws.Send("{\"m\":0,\"i\":0,\"n\":\"GetAccountPositions\",\"o\":\"{\\\"OMSId\\\":1,\\\"AccountId\\\":4}\"}");
+        }
+
+        public void GetAccountTrades(int accountId, int count, int startIndex)
+        {
+            ws.Send("{\"m\":0,\"i\":0,\"n\":\"GetAccountTrades\",\"o\":\"{\\\"OMSId\\\":1,\\\"AccountId\\\":4,\\\"Count\\\":50,\\\"StartIndex\\\":0}\"}");
+        }
+
+        public void SendOrder(int accountId, int clientOrderId, decimal quantity, decimal displayQuantity, bool useDisplayQuantity, decimal limitPrice, int orderIdOCO, int orderType, int pegPriceType, int instrumentId, decimal TrailingAmount, decimal limitOffset, bool side, decimal stopPrice, int timeInForce)
+        {
+            ws.Send("{\"m\":0,\"i\":0,\"n\":\"SendOrder\",\"o\":\"{\\\"OMSId\\\": 1,\\\"AccountId\\\": 4,\\\"ClientOrderId\\\": 99,\\\"Quantity\\\": 1,\\\"DisplayQuantity\\\": 0,\\\"UseDisplayQuantity\\\": true,\\\"LimitPrice\\\": 95,\\\"OrderIdOCO\\\": 0,\\\"OrderType\\\": 2,\\\"PegPriceType\\\": 1,\\\"InstrumentId\\\": 1,\\\"TrailingAmount\\\": 1.0,\\\"LimitOffset\\\": 2.0,\\\"Side\\\": 0,\\\"StopPrice\\\": 96,\\\"TimeInForce\\\": 1}\"}");
+        }
+
+        public void GetOrderStatus()
+        {
+            //ws.Send("{\"m\":0,\"i\":0,\"n\":\"GetOrderStatus\",\"o\":\"{\\\"OMSId\\\":1,\\\"AccountId\\\":4,\\\"OrderId\\\":12}\"}");
         }
     }
 }
