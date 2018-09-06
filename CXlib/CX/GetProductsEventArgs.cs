2fd1b868b8eba23d6bb1db3e8249dae5119e6eea
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-06 17:01:30 +0800

    Created GetProductsEventArgs class.

---
+++ CXlib/CX/GetProductsEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CXlib
+{
+    public class GetProductsEventArgs : EventArgs
+    {
+        public int SequenceNumber { get; internal set; }
+        public Product[] Products { get; internal set; }
+    }
+}
