72376b12fd365cb356a6328f5a983b400f95f142
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-07 09:56:34 +0800

    Updated messagetype class.

--- CXlib/CX/MessageType.cs
+++ CXlib/CX/MessageType.cs
@@ -8,11 +8,11 @@ namespace CXlib
 {
     public static class MessageType
     {
-        public static int Request { get => 0; }
-        public static int Reply { get => 1; }
-        public static int Subscribe { get => 2; }
-        public static int Event { get => 3; }
-        public static int Unsubscribe { get => 4; }
-        public static int Error { get => 5; }
+        public const int Request = 0;
+        public const int Reply = 1;
+        public const int Subscribe = 2;
+        public const int Event = 3;
+        public const int Unsubscribe = 4;
+        public const int Error = 5;
     }
 }
