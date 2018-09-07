e4bf94994a28290a450af56899acf31bdbdef8ca
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-07 09:33:11 +0800

    Create response class.

---
+++ CXlib/CX/Response.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CXlib
+{
+    class Response
+    {
+        [JsonProperty("result")]
+        public bool Result { get; internal set; }
+        [JsonProperty("errormessage")]
+        public string ErrorMessage { get; internal set; }
+        [JsonProperty("errorcode")]
+        public int ErrorCode { get; internal set; }
+        [JsonProperty("detail")]
+        public string Detail { get; internal set; }
+    }
+}
