7da4206eba15ddd44ddfb7dcd8d182bb76ee03da
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-15 20:12:48 +0800

    Create AccountTrade class.

---
+++ CXlib/CX/AccountTrade.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CXlib
+{
+    public class AccountTrade
+    {
+        [JsonProperty("TradeId")]
+        public int TradeId { get; internal set; }
+        [JsonProperty("OrderId")]
+        public int OrderId { get; internal set; }
+        [JsonProperty("AccountId")]
+        public int AccountId { get; internal set; }
+        [JsonProperty("ClientOrderId")]
+        public int ClientOrderId { get; internal set; }
+        [JsonProperty("InstrumentId")]
+        public int InstrumentId { get; internal set; }
+        [JsonProperty("Side")]
+        public string Side { get; internal set; }
+        [JsonProperty("Quantity")]
+        public decimal Quantity { get; internal set; }
+        [JsonProperty("Price")]
+        public decimal Price { get; internal set; }
+        [JsonProperty("Value")]
+        public decimal Value { get; internal set; }
+        [JsonProperty("TradeTime")]
+        public long TradeTime { get; internal set; }
+    }
+}
