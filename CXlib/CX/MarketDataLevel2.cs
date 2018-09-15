9a158a2d885c7605c67499b14cdae07e8a7b2715
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-15 20:17:08 +0800

    Create MarketDataLevel2 class.

---
+++ CXlib/CX/MarketDataLevel2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CXlib
+{
+    public class MarketDataLevel2
+    {
+        public int SequenceNumber { get; internal set; }
+        public int NumTraders { get; internal set; }
+        public long Timestamp { get; internal set; }
+        public int ChangeType { get; internal set; }
+        public decimal LastTradedPx { get; internal set; }
+        public int NumOrders { get; internal set; }
+        public decimal Price { get; internal set; }
+        public int InstrumenId { get; internal set; }
+        public decimal Quantity { get; internal set; }
+        public int Side { get; internal set; }
+    }
+}
