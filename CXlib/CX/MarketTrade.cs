425703d9e9af1fb444bd30db19bc4fee2dbea454
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-15 20:18:56 +0800

    Create MarketTrade class.

---
+++ CXlib/CX/MarketTrade.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CXlib
+{
+    public class MarketTrade
+    {
+        public int TradeId { get; internal set; }
+        public int InstrumentId { get; internal set; }
+        public decimal Quantity { get; internal set; }
+        public decimal Price { get; internal set; }
+        public long Timestamp { get; internal set; }
+        public int Direction { get; internal set; }
+        public int TakerSide { get; internal set; }
+    }
+}
